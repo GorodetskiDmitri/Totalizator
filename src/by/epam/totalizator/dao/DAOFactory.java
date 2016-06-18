63490bb6de5913a8bcbf22eccc99a5d0b4da2c6d
Gorodetski Dmitri <gorodok.by@tut.by>
2016-06-18 16:47:09 +0300

    Task08

--- src/by/epam/totalizator/dao/DAOFactory.java
+++ src/by/epam/totalizator/dao/DAOFactory.java
@@ -1,5 +1,30 @@
 package by.epam.totalizator.dao;
 
+import by.epam.totalizator.dao.impl.SqlAdminDAO;
+import by.epam.totalizator.dao.impl.SqlUserDAO;
+
 public class DAOFactory {
 
+	private AdminDAO adminDAO = new SqlAdminDAO();
+	private UserDAO userDAO = new SqlUserDAO();
+	
+	private DAOFactory() {
+		
+	}
+	
+	private static class Holder {
+		private static final DAOFactory INSTANCE = new DAOFactory();
+	}
+	
+	public static DAOFactory getInstance() {
+		return Holder.INSTANCE;
+	}
+	
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+	
+	public AdminDAO getAdminDAO() {
+		return adminDAO;
+	}
 }
