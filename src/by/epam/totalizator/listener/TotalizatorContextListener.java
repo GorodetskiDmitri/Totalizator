3df2f6f4b8182d5051cf42d7b85b34949ad3ed7a
gorodetski <gorodok.by@tut.by>
2016-10-11 14:33:40 +0200

    listener

--- src/by/epam/totalizator/listener/TotalizatorContextListener.java
+++ src/by/epam/totalizator/listener/TotalizatorContextListener.java
@@ -3,11 +3,16 @@ package by.epam.totalizator.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.Logger;
+
+import by.epam.totalizator.command.impl.ShowBetHistoryCommand;
 import by.epam.totalizator.dao.connectionpool.ConnectionPool;
 import by.epam.totalizator.dao.connectionpool.exception.ConnectionPoolException;
 
 public class TotalizatorContextListener implements ServletContextListener {
 
+	private static final Logger logger = Logger.getLogger(TotalizatorContextListener.class);
+	
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		ConnectionPool connectionPool = ConnectionPool.getInstance();
@@ -20,6 +25,7 @@ public class TotalizatorContextListener implements ServletContextListener {
 		try {
 			connectionPool.initPoolData();
 		} catch (ConnectionPoolException e) {
+			logger.error("Could not initialize connection pool");
 			throw new RuntimeException();
 		}
 	}
